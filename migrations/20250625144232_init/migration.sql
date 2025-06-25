a2369cddba0bebf308273b4fd2b0e8fe7f62026a
vincanger <70215737+vincanger@users.noreply.github.com>
2025-06-25 16:46:06 +0200

    db migrations

--- migrations/20250625144232_init/migration.sql
+++ migrations/20250625144232_init/migration.sql
@@ -1,11 +1,13 @@
 -- CreateTable
 CREATE TABLE "UserAnalysis" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "handle" TEXT NOT NULL,
     "situation" TEXT NOT NULL,
     "profileImageUrl" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "UserAnalysis_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
